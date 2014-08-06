f682f5e1ce333154ccd5f62ce6bb2365b9ceb28d
Nikolay Korolev <nikolay@aloha.pm>
2014-08-05 19:58:04 -0700

    Multiple ways to create threads.

--- threads.cc
+++ threads.cc
@@ -1,12 +1,24 @@
+#include <string>
 #include <iostream>
 #include <thread>
 
-void call_from_thread() {
-    std::cout << "Hello, World." << std::endl;
+void f() {
+    std::cout << "Thread with a function." << std::endl;
 }
 
+struct c {
+    explicit c(const std::string& s) : s_(s) {
+    }
+    void operator()() const {
+        std::cout << "Thread within a class: " << s_ << std::endl;
+    }
+    std::string s_;
+};
+
 int main() {
-    std::thread t1(call_from_thread);
-    t1.join();
+    std::thread(f).join();
+    std::thread(c("foo")).join();
+    std::thread(c("bar")).join();
+    std::cout << "Main thread." << std::endl;
     return 0;
 }
