1cc6d29a1130db543d2e9e032c57e08ddbc2eb7b
Nikolay Korolev <nikolay@aloha.pm>
2014-08-05 22:05:32 -0700

    Added reading.

--- handler.h
+++ handler.h
@@ -12,13 +12,29 @@ using boost::asio::ip::tcp;
 
 // `socket` should support dereferencing. Accepts pointer or unique_ptr<>.
 template <typename T> void handle_request_sync(T&& socket) {
-    for (int i = 1; i <= 10; ++i) {
+    boost::system::error_code ignored_error;
+    {
+        std::string message = "Type a string to echo first: ";
+        boost::asio::write(*socket, boost::asio::buffer(message), boost::asio::transfer_all(), ignored_error);
+    }
+    {
+        std::string s;
+        char c;
+        while (true) {
+            boost::asio::read(*socket, boost::asio::buffer(&c, 1), boost::asio::transfer_all(), ignored_error);
+            if (c == '\n' || c == '\r') {
+                break;
+            }
+            s += c;
+        }
+        s = "Echo: " + s + "\n";
+        boost::asio::write(*socket, boost::asio::buffer(s), boost::asio::transfer_all(), ignored_error);
+    }
+    for (int i = 5; i > 0; --i) {
         std::ostringstream os;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        os << "Output " << i << " "
-           << "\n";
+        os << "Done in " << i << " seconds.\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         std::string message = os.str();
-        boost::system::error_code ignored_error;
         boost::asio::write(*socket, boost::asio::buffer(message), boost::asio::transfer_all(), ignored_error);
     }
 }
